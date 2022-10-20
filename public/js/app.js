8b6745e7efd63cdf004eb3c384dc8856befdef44
Francesco Pieraccini <pieraccini.francesco@gmail.com>
2022-10-20 17:23:38 +0200

    npm

--- public/js/app.js
+++ public/js/app.js
@@ -19437,8 +19437,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\BooleanDevelopement\laravel-esercizio-team\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\BooleanDevelopement\laravel-esercizio-team\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\Users\franc\Documents\boolean-dev\full-stack-dev\php\esercitazioni\laravel-progetto-condiviso\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\Users\franc\Documents\boolean-dev\full-stack-dev\php\esercitazioni\laravel-progetto-condiviso\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
