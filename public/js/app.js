90b36592a1f8174bcec83748b98c5f446fac89a4
LuigiDiS <luigi.distefano69@gmail.com>
2022-10-20 16:30:37 +0200

    migration and seed

--- public/js/app.js
+++ public/js/app.js
@@ -49715,8 +49715,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\Users\franc\Documents\boolean-dev\full-stack-dev\php\esercitazioni\laravel-progetto-condiviso\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\Users\franc\Documents\boolean-dev\full-stack-dev\php\esercitazioni\laravel-progetto-condiviso\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\MAMP\htdocs\webdev\laravel-esercizio-team\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\MAMP\htdocs\webdev\laravel-esercizio-team\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
